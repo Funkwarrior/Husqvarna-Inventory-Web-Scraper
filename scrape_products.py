aa684118cfdd271e1e401efef2268141a1fa4335
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-02-16 00:25:07 +0100

    goin good

--- scrape_products.py
+++ scrape_products.py
@@ -2,6 +2,9 @@
 from lxml import html
 from bs4 import BeautifulSoup as soup
 import requests
+import ftfy
+import pandas as pd
+
 
 with open('ex_prodcat.html', 'r') as f:
 
@@ -10,16 +13,13 @@ with open('ex_prodcat.html', 'r') as f:
     type = page.find("h1").text
 
     products = page.find("div", {"class": "hui-grid__grid-lg-9"}).findAll("a", {"class": "hbd-link"})
-
     for product in products:
       if product.find('h4') is not None:
         product_name = product.find('h4').text
         product_link = product.get('href')
       # prints the dataset to console
-        print("name: " + str(product_name))
+        print("name: " + ftfy.fix_text(product_name))
         print("link: " + str(product_link))
-
         # writes the dataset to file
     # f.write(product_name + ", " + article_number.replace(",", "|") + ", " + price + imglink +"\n")
-
 f.close()
