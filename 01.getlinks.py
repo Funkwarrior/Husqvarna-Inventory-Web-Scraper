126602da4048e973f6080428998364ada50632b0
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-03-12 14:40:19 +0100

    victory?

--- 01.getlinks.py
+++ 01.getlinks.py
@@ -11,6 +11,9 @@ import json
 from itertools import chain
 import pandas as pd
 from io import BytesIO
+from styleframe import StyleFrame
+import re
+import time
 
 baseurl = "https://www.husqvarna.com"
 language = "it"
@@ -48,9 +51,25 @@ headers = {
     'accept-language': 'en-GB,en;q=0.9,it-IT;q=0.8,it;q=0.7,en-US;q=0.6',
     'cookie': 'sessionid=vq1dc4henrngbo1v1tmppumo; hsqglobal#lang=it-IT; ASP.NET_SessionId=q3xq25km0fovv1f5fnpeldcz; SC_ANALYTICS_GLOBAL_COOKIE=68f29e1e7f704e90805b059c6353b096|False; OptanonAlertBoxClosed=2022-02-10T13:05:36.776Z; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Feb+21+2022+17%3A17%3A07+GMT%2B0100+(Central+European+Standard+Time)&version=6.29.0&isIABGlobal=false&hosts=&consentId=59065fbc-8362-47f7-9fc9-7117155d9e71&interactionCount=1&landingPath=NotLandingPage&groups=C0002%3A1%2CC0003%3A1%2CC0004%3A1%2CC0005%3A1%2CC0001%3A1&geolocation=IT%3B36&AwaitingReconsent=false',
 }
+
 categories = []
 products_links = []
 product = []
+
+def get_prices(id):
+  json_data_for_price = {
+    'query': '\n    query getCommerceData($articleIds: [ID!]!, $productSkus: [ID!]!, $siteName: String!) {\n  site(name: $siteName) {\n    articles {\n      byIds(ids: $articleIds) {\n        ...ArticleCommerceData\n      }\n    }\n    products {\n      bySkus(skus: $productSkus) {\n        ...ProductCommerceData\n      }\n    }\n  }\n}\n    \n    fragment ArticleCommerceData on Article {\n  id\n  inventory\n  commerceLink {\n    ...LinkDataFields\n  }\n  campaignPrice {\n    ...PriceFragment\n  }\n  price {\n    ...PriceFragment\n  }\n  isNew\n  hasCampaigns\n  campaigns {\n    ...CampaignData\n  }\n  buyableOnline\n}\n    \n\n    fragment LinkDataFields on LinkData {\n  text\n  href\n  target\n  kind\n  title\n}\n    \n\n    fragment PriceFragment on Price {\n  disclaimer\n  displayPrice {\n    ...MoneyFragment\n  }\n}\n    \n\n    fragment MoneyFragment on Money {\n  amount\n  currency\n}\n    \n\n    fragment CampaignData on CampaignType {\n  title\n  description\n}\n    \n\n    fragment ProductCommerceData on Product {\n  sku\n  fromListPrice {\n    ...PriceFragment\n  }\n  fromCampaignPrice {\n    ...PriceFragment\n  }\n  isNew\n  hasCampaigns\n}\n    ',
+    'variables': {
+        'articleIds': id,
+        'productSkus': [],
+        'siteName': 'hbd-it-it-it',
+    },
+  }
+  response = requests.post('https://www.husqvarna.com/hbd/graphql', headers=headers, json=json_data_for_price)
+  data = response.json()
+  normalPrice = jmespath.search(f"data.site.articles.byIds[0].price.displayPrice.amount", data)
+  offerPrice = jmespath.search(f"data.site.articles.byIds[0].campaignPrice.displayPrice.amount", data)
+  return normalPrice, offerPrice
 
 def get_categories():
   try:
@@ -68,6 +87,7 @@ def get_categories():
 
 
 def scan_for_products_link():
+
   for products in categories:
     try:
       s = HTMLSession()
@@ -84,6 +104,8 @@ def scan_for_products_link():
         product_link = baseurl + item.get('href')
         product_name = ftfy.fix_text(item.find("h4").text)
         products_links.append({product_name, product_link})
+
+  time.sleep(1.5)
 
   return products_links
 
@@ -106,15 +128,39 @@ def get_product_details():
 
     json_clean = json.loads(json_dirty)
 
+    p_specs = []
+    p_specs_data = jmespath.search("initialData.site.products.get.articles[0].specificationValues", json_clean)
+    p_name = jmespath.search("initialData.site.products.get.name.longName", json_clean)
+    p_sku = jmespath.search("initialData.site.products.get.sku", json_clean)
+    p_url = jmespath.search("initialData.site.products.get.url", json_clean)
+    p_cat = jmespath.search("initialData.site.products.get.category.name", json_clean)
+    p_subcat = jmespath.search("initialData.site.products.get.subCategories[0].name", json_clean)
+    p_id = jmespath.search("initialData.site.products.get.articles[0].id", json_clean)
+    p_desc = jmespath.search("initialData.site.products.get.articles[0].introductionText", json_clean)
+    p_image = jmespath.search("initialData.site.products.get.articles[0].mainImage.sources[7].url", json_clean)
+    p_normalPrice, p_offerPrice = get_prices(p_id)
+
+
+    for i in range(len(p_specs_data)):
+      if p_specs_data[i].get("formattedValue") is not None:
+        p_specs.append(
+          p_specs_data[i]["specificationDefinitions"]["name"] + " " +
+          p_specs_data[i]["formattedValue"]
+        )
+
+    img_name = re.sub(r"\W+|_-", "_", p_id + "-" + p_name )+ '.jpg'
+
     product.append({
-      "p_name" : jmespath.search("initialData.site.products.get.name.longName", json_clean),
-      "p_sku" : jmespath.search("initialData.site.products.get.sku", json_clean),
-      "p_url" : baseurl+jmespath.search("initialData.site.products.get.url", json_clean),
-      "p_cat" : jmespath.search("initialData.site.products.get.category.name", json_clean),
-      "p_subcat" : jmespath.search("initialData.site.products.get.subCategories.name", json_clean),
-      "p_id" : jmespath.search("initialData.site.products.get.articles[0].id", json_clean),
-      "p_desc" : jmespath.search("initialData.site.products.get.articles[0].introductionText", json_clean),
-      "p_image" : jmespath.search("initialData.site.products.get.articles[0].mainImage.sources[7].url", json_clean),
+      "p_name" : p_name,
+      "p_sku" : p_sku,
+      "p_url" : baseurl+p_url,
+      "p_cat" : p_cat,
+      "p_subcat" : p_subcat,
+      "p_id" : p_id,
+      "p_desc" : "\n".join(p_specs) + "\n" + p_desc,
+      "p_image" : "'%PROFILE%\Documents\Images\\'" + img_name,
+      "p_normalPrice" : p_normalPrice,
+      "p_offerPrice" : p_offerPrice,
     })
 
     """
@@ -127,12 +173,10 @@ def get_product_details():
     img_name = product[i].get("p_id") + "-" + product[i].get("p_name").replace(" ", "_") + '.jpg'
     img_data.save(img_name,"JPEG")
     """
+    time.sleep(1.5)
 
   return product
 
 
-
-products_links = ["https://www.husqvarna.com/it/decespugliatori/129rj/","https://www.husqvarna.com/it/giocattoli/tagliasiepi-giocattolo/"]
-
 df = pd.DataFrame(get_product_details())
-df.to_csv("single_product.csv", index=False)
+StyleFrame(df).to_excel("single_product.xlsx", index=False, sheet_name="Prodotti Husqvarna").save()
--- 03.price.py
+++ 03.price.py
@@ -23,9 +23,9 @@ headers = {
     'accept-language': 'en-GB,en;q=0.9,it-IT;q=0.8,it;q=0.7,en-US;q=0.6',
     'cookie': 'sessionid=vq1dc4henrngbo1v1tmppumo; hsqglobal#lang=it-IT; ASP.NET_SessionId=q3xq25km0fovv1f5fnpeldcz; SC_ANALYTICS_GLOBAL_COOKIE=68f29e1e7f704e90805b059c6353b096|False; OptanonAlertBoxClosed=2022-02-10T13:05:36.776Z; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Feb+21+2022+17%3A17%3A07+GMT%2B0100+(Central+European+Standard+Time)&version=6.29.0&isIABGlobal=false&hosts=&consentId=59065fbc-8362-47f7-9fc9-7117155d9e71&interactionCount=1&landingPath=NotLandingPage&groups=C0002%3A1%2CC0003%3A1%2CC0004%3A1%2CC0005%3A1%2CC0001%3A1&geolocation=IT%3B36&AwaitingReconsent=false',
 }
-ids = ['967193401','586497901']
+ids = ['967193401','967893912']
 
-json_data = {
+json_data_for_price = {
     'query': '\n    query getCommerceData($articleIds: [ID!]!, $productSkus: [ID!]!, $siteName: String!) {\n  site(name: $siteName) {\n    articles {\n      byIds(ids: $articleIds) {\n        ...ArticleCommerceData\n      }\n    }\n    products {\n      bySkus(skus: $productSkus) {\n        ...ProductCommerceData\n      }\n    }\n  }\n}\n    \n    fragment ArticleCommerceData on Article {\n  id\n  inventory\n  commerceLink {\n    ...LinkDataFields\n  }\n  campaignPrice {\n    ...PriceFragment\n  }\n  price {\n    ...PriceFragment\n  }\n  isNew\n  hasCampaigns\n  campaigns {\n    ...CampaignData\n  }\n  buyableOnline\n}\n    \n\n    fragment LinkDataFields on LinkData {\n  text\n  href\n  target\n  kind\n  title\n}\n    \n\n    fragment PriceFragment on Price {\n  disclaimer\n  displayPrice {\n    ...MoneyFragment\n  }\n}\n    \n\n    fragment MoneyFragment on Money {\n  amount\n  currency\n}\n    \n\n    fragment CampaignData on CampaignType {\n  title\n  description\n}\n    \n\n    fragment ProductCommerceData on Product {\n  sku\n  fromListPrice {\n    ...PriceFragment\n  }\n  fromCampaignPrice {\n    ...PriceFragment\n  }\n  isNew\n  hasCampaigns\n}\n    ',
     'variables': {
         'articleIds': ids,
@@ -34,7 +34,7 @@ json_data = {
     },
 }
 
-response = requests.post('https://www.husqvarna.com/hbd/graphql', headers=headers, json=json_data)
+response = requests.post('https://www.husqvarna.com/hbd/graphql', headers=headers, json=json_data_for_price)
 data = response.json()
 
 for i in range(len(ids)):
